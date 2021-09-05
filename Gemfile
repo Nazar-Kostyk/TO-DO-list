4b7cbb8a166a3073e160fb4e70c071637ebbbe24
Nazar-Kostyk <kostyknazar1@gmail.com>
2021-09-06 02:01:58 +0300

    Configure rspec

--- Gemfile
+++ Gemfile
@@ -16,15 +16,8 @@ gem 'puma', '~> 5.0'
 group :development, :test do
   gem 'byebug', '>= 11.1.3', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails', '>= 6.2.0'
-  gem 'faker', '>= 2.19.0'
-  gem 'prosopite', '>= 1.0.1'
-  gem 'rails_best_practices', '>= 1.21.0'
-  gem 'rspec-rails', '~> 5.0.0'
-  gem 'rubocop', '>= 1.20.0', require: false
-  gem 'rubocop-performance', '>= 1.11.5'
-  gem 'rubocop-rails', '>= 2.11.3'
-  gem 'rubocop-rspec', '>= 2.4.0'
-  gem 'simplecov', '>= 0.21.2', require: false, group: :test
+  gem 'pry-byebug', '~> 3.9.0'
+  gem 'pry-rails', '~> 0.3.9'
 end
 
 group :development do
@@ -34,9 +27,19 @@ group :development do
   gem 'bundler-leak', '>= 0.2.0'
   gem 'lefthook', '>= 0.7.6'
   gem 'listen', '~> 3.3'
+  gem 'rails_best_practices', '>= 1.21.0'
+  gem 'rubocop', '>= 1.20.0', require: false
+  gem 'rubocop-performance', '>= 1.11.5'
+  gem 'rubocop-rails', '>= 2.11.3'
+  gem 'rubocop-rspec', '>= 2.4.0'
   gem 'spring', '>= 2.1.1'
 end
 
 group :test do
+  gem 'database_cleaner-active_record', '~> 2.0.1'
+  gem 'faker', '>= 2.19.0'
+  gem 'prosopite', '>= 1.0.1'
+  gem 'rspec-rails', '~> 5.0.0'
   gem 'shoulda-matchers', '~> 5.0'
+  gem 'simplecov', '>= 0.21.2', require: false, group: :test
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'spec_helper'
+
+ENV['RAILS_ENV'] ||= 'test'
+
 SimpleCov.start
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
+
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -36,9 +40,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -66,11 +67,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
 end
