226ac3a47dd50e8a19f8ce1e1d3887e3f3e78754
Nazar-Kostyk <kostyknazar1@gmail.com>
2021-10-01 13:04:50 +0300

    Add ToDoListsController specs

--- spec/controllers/to_do_lists_controller_spec.rb
+++ spec/controllers/to_do_lists_controller_spec.rb
@@ -58,15 +58,15 @@ RSpec.describe ToDoListsController, type: :request do
 
       context 'when id is valid' do
         let(:to_do_list_id) { user.to_do_lists.sample.id }
-  
+
         it 'returns correct response' do
           endpoint_call
-  
+
           expect(response).to be_ok
           expect(JSON.parse(response.body)).to match_json_schema('to_do_lists/single')
           expect(JSON.parse(response.body)['data']['id']).to eq(to_do_list_id)
         end
-  
+
         context 'when to-do list not found' do
           let(:to_do_list_id) { SecureRandom.uuid }
           let(:expected_body) do
@@ -80,10 +80,10 @@ RSpec.describe ToDoListsController, type: :request do
               ]
             }
           end
-  
+
           it 'returns not_found' do
             endpoint_call
-  
+
             expect(response).to be_not_found
             expect(JSON.parse(response.body)).to eq(expected_body)
           end
@@ -168,7 +168,7 @@ RSpec.describe ToDoListsController, type: :request do
           before do
             allow_any_instance_of(ToDoList).to receive(:save).and_return(nil)
           end
-  
+
           let(:expected_body) do
             {
               'errors' => [
@@ -180,10 +180,10 @@ RSpec.describe ToDoListsController, type: :request do
               ]
             }
           end
-  
+
           it 'returns the bad request' do
             endpoint_call
-  
+
             expect(response).to have_http_status(:unprocessable_entity)
             expect(JSON.parse(response.body)).to eq(expected_body)
           end
@@ -191,15 +191,25 @@ RSpec.describe ToDoListsController, type: :request do
       end
 
       context 'when params are invalid' do
-        let(:params) { { title: Faker::Lorem.characters(number: ToDoList::TITLE_MAX_LENGTH + 1), description: Faker::Lorem.paragraph } }
+        let(:params) do
+          {
+            title: Faker::Lorem.characters(number: ToDoList::TITLE_MAX_LENGTH + 1),
+            description: Faker::Lorem.paragraph
+          }
+        end
         let(:expected_body) do
           {
             'errors' => [
               {
                 'source' => {
-                  'pointer' => "/data/attributes/title"
+                  'pointer' => '/data/attributes/title'
                 },
-                'detail' => I18n.t('dry_validation.errors.exceeds_maximum_length', field: :title, length: ToDoList::TITLE_MAX_LENGTH)
+                'detail' =>
+                  I18n.t(
+                    'dry_validation.errors.exceeds_maximum_length',
+                    field: :title,
+                    length: ToDoList::TITLE_MAX_LENGTH
+                  )
               }
             ]
           }
@@ -217,6 +227,241 @@ RSpec.describe ToDoListsController, type: :request do
     context 'when Authorization header is invalid' do
       let(:authorization_header) { Faker::Lorem.characters(number: 128) }
       let(:params) { {} }
+      let(:expected_body) do
+        {
+          'errors' => [
+            {
+              'code' => 401,
+              'title' => I18n.t('error_messages.unauthorized_request.title'),
+              'detail' => I18n.t('error_messages.unauthorized_request.detail')
+            }
+          ]
+        }
+      end
+
+      it 'returns the unauthorized request' do
+        endpoint_call
+
+        expect(response).to be_unauthorized
+        expect(JSON.parse(response.body)).to eq(expected_body)
+      end
+    end
+  end
+
+  describe '#update' do
+    let(:endpoint_call) do
+      headers = { 'Authorization' => authorization_header }
+      put to_do_list_path(to_do_list_id), params: params, headers: headers
+    end
+
+    context 'when Authorization header is valid' do
+      let!(:user) { create(:user_with_to_do_lists) }
+      let(:authorization_header) { JsonWebToken.encode(user_id: user.id) }
+
+      context 'when params are valid' do
+        let(:to_do_list_id) { user.to_do_lists.sample.id }
+        let(:params) { { title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph } }
+
+        it 'returns correct response' do
+          endpoint_call
+
+          expect(response).to be_ok
+          expect(JSON.parse(response.body)).to match_json_schema('to_do_lists/single')
+        end
+
+        context 'when database error occured' do
+          before do
+            allow_any_instance_of(ToDoList).to receive(:update).and_return(nil)
+          end
+
+          let(:expected_body) do
+            {
+              'errors' => [
+                {
+                  'code' => 422,
+                  'title' => I18n.t('error_messages.database_error.title'),
+                  'detail' => I18n.t('error_messages.database_error.detail')
+                }
+              ]
+            }
+          end
+
+          it 'returns the bad request' do
+            endpoint_call
+
+            expect(response).to have_http_status(:unprocessable_entity)
+            expect(JSON.parse(response.body)).to eq(expected_body)
+          end
+        end
+      end
+
+      context 'when params are invalid' do
+        let(:to_do_list_id) { 'invalid' }
+        let(:params) do
+          {
+            title: Faker::Lorem.characters(number: ToDoList::TITLE_MAX_LENGTH + 1),
+            description: Faker::Lorem.paragraph
+          }
+        end
+        let(:expected_body) do
+          {
+            'errors' => [
+              {
+                'source' => {
+                  'pointer' => '/data/attributes/id'
+                },
+                'detail' => I18n.t('dry_validation.errors.invalid_format', field: 'id', format_name: :UUID)
+              },
+              {
+                'source' => {
+                  'pointer' => '/data/attributes/title'
+                },
+                'detail' =>
+                  I18n.t(
+                    'dry_validation.errors.exceeds_maximum_length',
+                    field: :title,
+                    length: ToDoList::TITLE_MAX_LENGTH
+                  )
+              }
+            ]
+          }
+        end
+
+        it 'returns correct response' do
+          endpoint_call
+
+          expect(response).to be_bad_request
+          expect(JSON.parse(response.body)).to eq(expected_body)
+        end
+      end
+    end
+
+    context 'when Authorization header is invalid' do
+      let!(:user) { create(:user_with_to_do_lists) }
+      let(:to_do_list_id) { user.to_do_lists.sample.id }
+      let(:params) { {} }
+      let(:authorization_header) { Faker::Lorem.characters(number: 128) }
+      let(:expected_body) do
+        {
+          'errors' => [
+            {
+              'code' => 401,
+              'title' => I18n.t('error_messages.unauthorized_request.title'),
+              'detail' => I18n.t('error_messages.unauthorized_request.detail')
+            }
+          ]
+        }
+      end
+
+      it 'returns the unauthorized request' do
+        endpoint_call
+
+        expect(response).to be_unauthorized
+        expect(JSON.parse(response.body)).to eq(expected_body)
+      end
+    end
+  end
+
+  describe '#destroy' do
+    let(:endpoint_call) do
+      headers = { 'Authorization' => authorization_header }
+      delete to_do_list_path(to_do_list_id), headers: headers
+    end
+
+    context 'when Authorization header is valid' do
+      let!(:user) { create(:user_with_to_do_lists) }
+      let(:authorization_header) { JsonWebToken.encode(user_id: user.id) }
+
+      context 'when id is valid' do
+        let(:to_do_list_id) { user.to_do_lists.sample.id }
+
+        it 'deletes to-do list' do
+          expect { endpoint_call }.to change(ToDoList, :count).by(-1)
+        end
+
+        it 'returns correct response' do
+          endpoint_call
+
+          expect(response).to be_no_content
+          expect(response.body).to be_empty
+        end
+
+        context 'when to-do list not found' do
+          let(:to_do_list_id) { SecureRandom.uuid }
+          let(:expected_body) do
+            {
+              'errors' => [
+                {
+                  'code' => 404,
+                  'title' => I18n.t('error_messages.to_do_list_not_found.title'),
+                  'detail' => I18n.t('error_messages.to_do_list_not_found.detail')
+                }
+              ]
+            }
+          end
+
+          it 'returns not_found' do
+            endpoint_call
+
+            expect(response).to be_not_found
+            expect(JSON.parse(response.body)).to eq(expected_body)
+          end
+        end
+
+        context 'when database error occured' do
+          before do
+            allow_any_instance_of(ToDoList).to receive(:destroy).and_return(nil)
+          end
+
+          let(:expected_body) do
+            {
+              'errors' => [
+                {
+                  'code' => 422,
+                  'title' => I18n.t('error_messages.database_error.title'),
+                  'detail' => I18n.t('error_messages.database_error.detail')
+                }
+              ]
+            }
+          end
+
+          it 'returns the bad request' do
+            endpoint_call
+
+            expect(response).to have_http_status(:unprocessable_entity)
+            expect(JSON.parse(response.body)).to eq(expected_body)
+          end
+        end
+      end
+
+      context 'when id is invalid' do
+        let(:to_do_list_id) { 'invalid' }
+        let(:expected_body) do
+          {
+            'errors' => [
+              {
+                'source' => {
+                  'pointer' => '/data/attributes/id'
+                },
+                'detail' => I18n.t('dry_validation.errors.invalid_format', field: 'id', format_name: :UUID)
+              }
+            ]
+          }
+        end
+
+        it 'returns the bad request' do
+          endpoint_call
+
+          expect(response).to be_bad_request
+          expect(JSON.parse(response.body)).to eq(expected_body)
+        end
+      end
+    end
+
+    context 'when Authorization header is invalid' do
+      let!(:user) { create(:user_with_to_do_lists) }
+      let(:to_do_list_id) { user.to_do_lists.sample.id }
+      let(:authorization_header) { Faker::Lorem.characters(number: 128) }
       let(:expected_body) do
         {
           'errors' => [
