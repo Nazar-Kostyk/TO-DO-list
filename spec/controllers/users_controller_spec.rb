aa915caa79c13c25b3926593ff7e7a0ba2e69888
Nazar-Kostyk <kostyknazar1@gmail.com>
2021-10-20 19:06:19 +0300

    Update specs for UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,32 +6,21 @@ RSpec.describe UsersController, type: :request do
   end
 
   describe '#show' do
+    let!(:user) { create(:user) }
+    let(:authorization_header) { JWTSessions::Session.new(payload: { user_id: user.id }).login[:access] }
+    let(:headers) { { 'Authorization' => authorization_header } }
+
     let(:endpoint_call) do
-      headers = { 'Authorization' => authorization_header }
-      get user_path, headers: headers
+      get user_path, headers: headers, as: :json
     end
 
-    let!(:user) { create(:user) }
-
-    context 'when Authorization header is valid' do
-      let(:authorization_header) { JWTSessions::Session.new(payload: { user_id: user.id }).login[:access] }
-
+    context 'when params are valid' do
       it 'returns correct response' do
         endpoint_call
 
         expect(response).to be_ok
         expect(JSON.parse(response.body)).to match_json_schema('users/single')
       end
-    end
-
-    context 'when Authorization header is invalid' do
-      before do
-        endpoint_call
-      end
-
-      let(:authorization_header) { 'invalid' }
-
-      it_behaves_like 'unauthorized request'
     end
   end
 
@@ -40,7 +29,7 @@ RSpec.describe UsersController, type: :request do
       post user_path, params: params
     end
 
-    context 'when valid params provided' do
+    context 'when params are valid' do
       let(:password) { Faker::Lorem.characters(number: User::PASSWORD_MIN_LENGTH) }
       let(:params) do
         {
@@ -62,142 +51,60 @@ RSpec.describe UsersController, type: :request do
         expect(response).to be_created
         expect(JSON.parse(response.body)).to match_json_schema('users/single')
       end
-
-      context 'when database error occured' do
-        before do
-          allow_any_instance_of(User).to receive(:save).and_return(nil)
-          endpoint_call
-        end
-
-        it_behaves_like 'database error'
-      end
     end
 
-    context 'when invalid params provided' do
-      context 'when required params are missing' do
-        let(:params) { {} }
-        let(:expected_body) do
-          {
-            'errors' =>
-              %i[name surname email password password_confirmation].map do |attribute|
-                {
-                  'source' => {
-                    'pointer' => "/data/attributes/#{attribute}"
-                  },
-                  'detail' => I18n.t('dry_validation.errors.key?')
-                }
-              end
-          }
-        end
+    context 'when params are invalid' do
+      let(:params) { {} }
 
-        it 'returns the bad request' do
-          endpoint_call
+      it 'returns correct response' do
+        endpoint_call
 
-          expect(response).to be_bad_request
-          expect(JSON.parse(response.body)).to eq(expected_body)
-        end
+        expect(response).to be_bad_request
+        expect(JSON.parse(response.body)).to match_json_schema('incorrect_response/validation_errors')
       end
     end
   end
 
   describe '#update' do
-    let(:endpoint_call) do
-      headers = { 'Authorization' => authorization_header }
-      put user_path, params: params, headers: headers
-    end
-
     let(:password) { Faker::Lorem.characters(number: User::PASSWORD_MIN_LENGTH) }
     let!(:user) { create(:user, password: password) }
+    let(:authorization_header) { JWTSessions::Session.new(payload: { user_id: user.id }).login[:access] }
+    let(:headers) { { 'Authorization' => authorization_header } }
 
-    context 'when Authorization header is valid' do
-      let(:authorization_header) { JWTSessions::Session.new(payload: { user_id: user.id }).login[:access] }
+    let(:endpoint_call) do
+      put user_path, params: params, headers: headers, as: :json
+    end
 
-      context 'when valid params provided' do
-        let(:new_password) { Faker::Lorem.characters(number: User::PASSWORD_MIN_LENGTH) }
-        let(:params) do
-          {
-            name: Faker::Name.first_name,
-            surname: Faker::Name.last_name,
-            email: Faker::Internet.email,
-            current_password: password,
-            new_password: new_password,
-            new_password_confirmation: new_password
-          }
-        end
-
-        it 'returns correct response' do
-          endpoint_call
-
-          expect(response).to be_ok
-          expect(JSON.parse(response.body)).to match_json_schema('users/single')
-        end
-
-        context 'when database error occured' do
-          before do
-            allow_any_instance_of(User).to receive(:update).and_return(nil)
-            endpoint_call
-          end
-
-          it_behaves_like 'database error'
-        end
+    context 'when params are valid' do
+      let(:new_password) { Faker::Lorem.characters(number: User::PASSWORD_MIN_LENGTH) }
+      let(:params) do
+        {
+          name: Faker::Name.first_name,
+          surname: Faker::Name.last_name,
+          email: Faker::Internet.email,
+          current_password: password,
+          new_password: new_password,
+          new_password_confirmation: new_password
+        }
       end
 
-      context 'when invalid params provided' do
-        context 'when required field is missing' do
-          let(:params) { {} }
-          let(:expected_body) do
-            {
-              'errors' => [
-                {
-                  'source' => {
-                    'pointer' => '/data/attributes/current_password'
-                  },
-                  'detail' => I18n.t('dry_validation.errors.key?')
-                }
-              ]
-            }
-          end
+      it 'returns correct response' do
+        endpoint_call
 
-          it 'returns the bad request' do
-            endpoint_call
-
-            expect(response).to be_bad_request
-            expect(JSON.parse(response.body)).to eq(expected_body)
-          end
-        end
-
-        context 'when current_password is invalid' do
-          let(:params) { { current_password: Faker::Lorem.characters(number: User::PASSWORD_MIN_LENGTH) } }
-          let(:expected_body) do
-            {
-              'errors' => [
-                {
-                  'title' => I18n.t('error_messages.wrong_password.title'),
-                  'detail' => I18n.t('error_messages.wrong_password.detail')
-                }
-              ]
-            }
-          end
-
-          it 'returns the bad request' do
-            endpoint_call
-
-            expect(response).to be_unauthorized
-            expect(JSON.parse(response.body)).to eq(expected_body)
-          end
-        end
+        expect(response).to be_ok
+        expect(JSON.parse(response.body)).to match_json_schema('users/single')
       end
     end
 
-    context 'when Authorization header is invalid' do
-      before do
-        endpoint_call
-      end
-
-      let(:authorization_header) { 'invalid' }
+    context 'when params are invalid' do
       let(:params) { {} }
 
-      it_behaves_like 'unauthorized request'
+      it 'returns correct response' do
+        endpoint_call
+
+        expect(response).to be_bad_request
+        expect(JSON.parse(response.body)).to match_json_schema('incorrect_response/validation_errors')
+      end
     end
   end
 end
